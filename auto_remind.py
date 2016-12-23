c7e577c1363df81dcb944477acf30f65d5f3cc0f
weizongwei5 <weizongwei5@gmail.com>
2016-12-23 17:48:24 +0800

    shell autocommit

--- auto_remind.py
+++ auto_remind.py
@@ -18,10 +18,11 @@ if(len(remind_text)==0):
 i = 1
 while (i <= 15):
     time.sleep(1)
-    str = raw_input("\n\t"+remind_text)
+    str = raw_input("\n\t"+remind_text+"\n\t")
     if (str == "y" or str == "yes"):
         i = 15
         print("\n\t next second will exit!!")
+        time.sleep(1)
 
     i += 1
 
